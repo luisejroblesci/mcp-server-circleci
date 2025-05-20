8e6ba936097343be26ab7bea17033f925188856c
Nic Diaz <nic.diaz@circleci.com>
2025-05-20 09:56:18 -0400

    improve handler.test

--- src/tools/rerunWorkflow/handler.test.ts
+++ src/tools/rerunWorkflow/handler.test.ts
@@ -4,79 +4,93 @@ import * as client from '../../clients/client.js';
 
 vi.mock('../../clients/client.js');
 
+const failedWorkflowId = '00000000-0000-0000-0000-000000000000';
+const successfulWorkflowId = '11111111-1111-1111-1111-111111111111';
+const newWorkflowId = '11111111-1111-1111-1111-111111111111';
+
 describe('rerunWorkflow', () => {
   beforeEach(() => {
     vi.resetAllMocks();
   });
 
-  it('should return the new workflowId and url to the user when the return is successful', async () => {
-    const mockCircleCIClient = {
-      workflows: {
-        getWorkflow: vi.fn().mockResolvedValue({ status: 'failed' }),
-        rerunWorkflow: vi.fn().mockResolvedValue({
-          workflow_id: '11111111-1111-1111-1111-111111111111',
-        }),
-      },
-    };
-
-    vi.spyOn(client, 'getCircleCIClient').mockReturnValue(
-      mockCircleCIClient as any,
-    );
-
-    const controller = new AbortController();
-    const result = await rerunWorkflow(
-      {
-        params: {
-          workflowId: '00000000-0000-0000-0000-000000000000',
+  describe('when rerunning a failed workflow', () => {
+    it('should return the new workflowId and url to the user', async () => {
+      const mockCircleCIClient = {
+        workflows: {
+          getWorkflow: vi.fn().mockResolvedValue({ status: 'failed' }),
+          rerunWorkflow: vi.fn().mockResolvedValue({
+            workflow_id: newWorkflowId,
+          }),
         },
-      },
-      {
-        signal: controller.signal,
-      },
-    );
+      };
 
-    expect(result).toEqual({
-      content: [
+      vi.spyOn(client, 'getCircleCIClient').mockReturnValue(
+        mockCircleCIClient as any,
+      );
+
+      const controller = new AbortController();
+      const result = await rerunWorkflow(
         {
-          type: 'text',
-          text: 'New workflowId is 11111111-1111-1111-1111-111111111111 and [View Workflow in CircleCI](https://app.circleci.com/pipelines/workflows/11111111-1111-1111-1111-111111111111)',
+          params: {
+            workflowId: failedWorkflowId,
+          },
         },
-      ],
+        {
+          signal: controller.signal,
+        },
+      );
+
+      expect(mockCircleCIClient.workflows.rerunWorkflow).toHaveBeenCalledWith({
+        workflowId: failedWorkflowId,
+        fromFailed: true,
+      });
+      expect(result).toEqual({
+        content: [
+          {
+            type: 'text',
+            text: `New workflowId is ${newWorkflowId} and [View Workflow in CircleCI](https://app.circleci.com/pipelines/workflows/11111111-1111-1111-1111-111111111111)`,
+          },
+        ],
+      });
     });
   });
-  it('returns an error if workflow status is success and fromFailed is true', async () => {
-    const mockCircleCIClient = {
-      workflows: {
-        getWorkflow: vi.fn().mockResolvedValue({ status: 'success' }),
-        rerunWorkflow: vi.fn(),
-      },
-    };
 
-    vi.spyOn(client, 'getCircleCIClient').mockReturnValue(
-      mockCircleCIClient as any,
-    );
-
-    const controller = new AbortController();
-    const response = await rerunWorkflow(
-      {
-        params: {
-          workflowId: '11111111-1111-1111-1111-111111111111',
-          fromFailed: true,
+  describe('when rerunning a successful workflow', () => {
+    it('should return an error if requested to be rerun from failed', async () => {
+      const mockCircleCIClient = {
+        workflows: {
+          getWorkflow: vi.fn().mockResolvedValue({ status: 'success' }),
+          rerunWorkflow: vi.fn(),
         },
-      },
-      {
-        signal: controller.signal,
-      },
-    );
+      };
 
-    expect(response).toEqual({
-      isError: true,
-      content: [
+      vi.spyOn(client, 'getCircleCIClient').mockReturnValue(
+        mockCircleCIClient as any,
+      );
+
+      const controller = new AbortController();
+      const response = await rerunWorkflow(
         {
-          type: 'text',
-          text: 'Workflow is not failed, cannot rerun from failed',
+          params: {
+            workflowId: successfulWorkflowId,
+            fromFailed: true,
+          },
         },
-      ],
+        {
+          signal: controller.signal,
+        },
+      );
+
+      expect(mockCircleCIClient.workflows.rerunWorkflow).not.toHaveBeenCalled();
+      expect(response).toEqual({
+        isError: true,
+        content: [
+          {
+            type: 'text',
+            text: 'Workflow is not failed, cannot rerun from failed',
+          },
+        ],
+      });
     });
   });
 });
