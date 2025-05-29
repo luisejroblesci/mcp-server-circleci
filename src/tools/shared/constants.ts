69a1dd4d96cb643416fadbfa86d39c6f1b19601c
ryan-circleci <ryan.hamilton@circleci.com>
2025-05-29 15:11:15 -0400

    chore: nit `// SHARED CIRCLECI TOOL DESCRIPTION CONSTANTS`

--- src/tools/shared/constants.ts
+++ src/tools/shared/constants.ts
@@ -1,4 +1,4 @@
-// SHARED CIRCLECI TOOL DESCRIPTIONS
+// SHARED CIRCLECI TOOL DESCRIPTION CONSTANTS
 export const projectSlugDescription = `The project slug from listFollowedProjects tool (e.g., "gh/organization/project"). When using this option, branch must also be provided.`;
 export const projectSlugDescriptionNoBranch = `The project slug from listFollowedProjects tool (e.g., "gh/organization/project").`;
 export const branchDescription = `The name of the branch currently checked out in local workspace. This should match local git branch. For example: "feature/my-branch", "bugfix/123", "main", "master" etc.`;
